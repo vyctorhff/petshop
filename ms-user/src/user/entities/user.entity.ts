c05a7b2248c2f0a76d8bb1115654836dd8ab29df
vyctorhff <vyctorhff@gmail.com>
2023-04-10 14:24:28 -0300

    feat(ms-user): adding user module

---
+++ ms-user/src/user/entities/user.entity.ts
@@ -0,0 +1,11 @@
+import { Perfil } from "src/perfil/entities/perfil.entity";
+import { JoinColumn, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+
+export class User {
+  @PrimaryGeneratedColumn()
+  id: string
+
+  @ManyToMany(() => Perfil, (perfil) => perfil.users)
+  @JoinColumn()
+  perfils: Perfil[];
+}
