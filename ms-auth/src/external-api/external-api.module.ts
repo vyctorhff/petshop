9ae9e40a9c862e1bd9e5a255005ecf3e1f3ca978
vyctorhff <vyctorhff@gmail.com>
2023-05-31 12:08:18 -0300

    ms-auth: add call to ms-user

--- ms-auth/src/external-api/external-api.module.ts
+++ ms-auth/src/external-api/external-api.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExternalApiService } from './external-api.service';
 import { UserExternalService } from './user-external.service';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [ConfigModule, HttpModule],
   providers: [ExternalApiService, UserExternalService],
   exports: [ExternalApiService, UserExternalService],
 })
--- ms-auth/src/external-api/user-external.service.ts
+++ ms-auth/src/external-api/user-external.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+
+class UserLoginDto {}
+
 
 @Injectable()
 export class UserExternalService {
-  constructor() {}
+  constructor(
+    private configService: ConfigService,
+    private httpService: HttpService,
+  ) {}
 
-  getUserByEnrollmentAndPass(enrollment: string, pass: string) {
-    return undefined;
+  async getUserByEnrollmentAndPass(enrollment: string, pass: string): Promise<UserLoginDto> {
+    const url = `${this.configService.get<string>('MS_USER')}/user`
+    const payload = {
+      enrollment,
+      pass
+    };
+
+    const postObserver = this.httpService.post<UserLoginDto>(url, payload);
+    const response = await firstValueFrom(postObserver.pipe())
+
+    return response.data;
   }
 }
