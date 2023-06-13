2a3316567ec59e3eb99e3bc7df651097a4d205ba
vyctorhff <vyctorhff@gmail.com>
2023-06-13 14:00:16 -0300

    feat(ms-user): fix migration

--- ms-user/src/migrations/1686530598229-AddRelationUserAndPerfil.ts
+++ ms-user/src/migrations/1686530598229-AddRelationUserAndPerfil.ts
@@ -3,18 +3,19 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 import { DataNames } from "./dataNames";
 
 const userTable = DataNames.USER.toString();
+const perfilTable = DataNames.PERFIL.toString();
 const userPerfilTable = DataNames.USER_PERFIL.toString();
 
 export class AddRelationUserAndPerfil1686530598229 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            CREATE TABLE ${DataNames.USER_PERFIL.toString()}(
+            CREATE TABLE ${userPerfilTable}(
                 id_user INT,
                 id_perfil INT,
 
-                CONSTRAINT fk_user FOREIGN KEY (id_user) REFERENCES ${userTable} (id)",
-                CONSTRAINT fk_perfil FOREIGN KEY (id_perfil) REFERENCES ${userPerfilTable} (id)",
+                CONSTRAINT fk_user FOREIGN KEY (id_user) REFERENCES ${userTable} (id),
+                CONSTRAINT fk_perfil FOREIGN KEY (id_perfil) REFERENCES ${perfilTable} (id)
             );
         `);
     }
