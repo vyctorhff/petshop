433dbd964a7c3635e6b5349976ec8e47438b825c
vyctorhff <vyctorhff@gmail.com>
2023-07-11 14:00:20 -0300

    feat(ms-auth): migration to create a user

--- ms-auth/src/migrations/1687028862966-CreateAuthentication.ts
+++ ms-auth/src/migrations/1687028862966-CreateAuthentication.ts
@@ -1,23 +1,21 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
-import { DataNames } from "./dataNames";
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { DataNames } from './dataNames';
 
 const tableAuthentication = DataNames.AUTHENTICATION.toString();
 
 export class CreateAuthentication1687028862966 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        const sql = `
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const sql = `
             CREATE TABLE ${tableAuthentication}(
                 id varchar(50),
                 key varchar(10),
                 password varchar(50)
             );
         `;
-        queryRunner.query(sql);
-    }
+    queryRunner.query(sql);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.query(`DROP TABLE ${tableAuthentication};`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`DROP TABLE ${tableAuthentication};`);
+  }
 }
