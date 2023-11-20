a37a0622d23c8b89616ae9cebe64fb4e0e1ace52
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-11-20 14:59:03 -0300

    fix(ms-auth): config spring url

--- ms-auth/src/main/java/br/com/petshop/auth/config/SecurityConfig.java
+++ ms-auth/src/main/java/br/com/petshop/auth/config/SecurityConfig.java
@@ -22,10 +22,10 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(registry -> {
-                registry.anyRequest().authenticated();
-
                 registry.requestMatchers(HttpMethod.POST, "/auth/v1").permitAll();
                 registry.requestMatchers(HttpMethod.GET, "/auth/v1").permitAll();
+
+                registry.anyRequest().authenticated();
             })
             .build();
     }
