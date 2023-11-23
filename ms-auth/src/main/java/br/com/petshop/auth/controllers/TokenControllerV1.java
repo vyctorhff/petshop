5d72d74724f329e7e6c638d5a96c77eb3f73d68a
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-11-23 09:51:27 -0300

    feat(ms-auth): add token controller

---
+++ ms-auth/src/main/java/br/com/petshop/auth/controllers/TokenControllerV1.java
@@ -0,0 +1,37 @@
+package br.com.petshop.auth.controllers;
+
+import br.com.petshop.auth.model.dto.TokenRequestDTO;
+import br.com.petshop.auth.model.dto.TokenResponseDTO;
+import br.com.petshop.auth.service.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/token")
+@RequiredArgsConstructor
+@Slf4j
+@Tag(name = "Endpoint for token")
+public class TokenControllerV1 {
+
+    private final TokenService tokenService;
+
+    @GetMapping
+    @Operation(summary = "Return token for authenticated user")
+    public ResponseEntity<TokenResponseDTO> getToken(@RequestBody TokenRequestDTO dto) {
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    @Operation(summary = "Refresh token")
+    public ResponseEntity<TokenResponseDTO> refresh(@RequestBody TokenRequestDTO dto) {
+        return ResponseEntity.ok().build();
+    }
+}
