9187d9eaa9d7d28067dcf15e760387f845591722
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-11-24 14:22:20 -0300

    feat(ms-auth): add delete user

--- ms-auth/src/main/java/br/com/petshop/auth/service/DeleteAuthenticationService.java
+++ ms-auth/src/main/java/br/com/petshop/auth/service/DeleteAuthenticationService.java
@@ -1,11 +1,25 @@
 package br.com.petshop.auth.service;
 
-import org.apache.commons.lang3.NotImplementedException;
+import br.com.petshop.auth.infra.security.UserRepository;
+import br.com.petshop.auth.model.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class DeleteAuthenticationService {
+
+    private final UserRepository repository;
+
     public void process(String enrollment) {
-        throw new NotImplementedException();
+        User user = repository.findByEnrollment(enrollment);
+
+        if (user == null) {
+            return;
+        }
+
+        repository.delete(user);
     }
 }
