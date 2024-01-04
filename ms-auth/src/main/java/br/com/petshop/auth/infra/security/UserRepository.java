4fb26e7acfa3f2e674b1cfb27901e27ab969f1ba
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-01-04 09:17:54 -0300

    feat(ms-auth): add query for user enrollment

--- ms-auth/src/main/java/br/com/petshop/auth/infra/security/UserRepository.java
+++ ms-auth/src/main/java/br/com/petshop/auth/infra/security/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, String> {
 
     User findByEnrollment(String enrollment);
+
+    boolean existsEnrollemnt(String enrollment);
 }
