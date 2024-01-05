208b802545f573fd2b27df8e2b9174b101d64f32
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-01-05 08:06:16 -0300

    feat(ms-auth): create user

--- ms-auth/src/main/java/br/com/petshop/auth/service/CreateAuthenticationService.java
+++ ms-auth/src/main/java/br/com/petshop/auth/service/CreateAuthenticationService.java
@@ -1,11 +1,15 @@
 package br.com.petshop.auth.service;
 
 import br.com.petshop.auth.infra.security.UserRepository;
+import br.com.petshop.auth.model.User;
 import br.com.petshop.auth.model.dto.CreateAuthenticationRequestDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -14,12 +18,23 @@ public class CreateAuthenticationService {
 
     private final UserRepository repository;
 
-    private final AuthenticationManager authenticationManager;
-
     public void create(CreateAuthenticationRequestDTO dto) {
         log.info("Creating authentication");
-        // TODO: check if enrollment exists
-        // TODO: test only admin quem process this
+
+        User entity = dto.toEntity();
+
+        if (repository.existsEnrollemnt(entity.getEnrollment())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Enrollment already exists");
+        }
+
+        if (entity.hasRoles()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "missing roles");
+        }
+
+        if (entity.hasAdminRole()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "it cannot has admin role");
+        }
+
         repository.save(dto.toEntity());
     }
 }
