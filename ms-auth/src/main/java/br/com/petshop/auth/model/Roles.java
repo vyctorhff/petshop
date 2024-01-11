5116c7b2c8975e65f4f8078c2e06e5d9b1159a1d
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-01-11 11:10:12 -0300

    feat(ms-auth): impruve Roles enum

--- ms-auth/src/main/java/br/com/petshop/auth/model/Roles.java
+++ ms-auth/src/main/java/br/com/petshop/auth/model/Roles.java
@@ -1,5 +1,26 @@
 package br.com.petshop.auth.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum Roles {
-    ADMIN, USER, EMPLOYEE;
+    ADMIN("admin"),
+    USER("user"),
+    EMPLOYEE("employee");
+
+    private final String name;
+
+    Roles(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static List<String> toStringList() {
+        return Arrays.stream(Roles.values())
+            .map(value -> value.getName())
+            .toList();
+    }
 }
