e6c594829fe32dee410c28df1d694a8bbe097b29
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-01-12 16:29:31 -0300

    feat(ms-auth): migration for user admin

--- ms-auth/src/main/resources/db/migration/V1_0_7__INSERT_USER_ADMIN.sql
+++ ms-auth/src/main/resources/db/migration/V1_0_7__INSERT_USER_ADMIN.sql
@@ -2,7 +2,6 @@ INSERT INTO tb_user
 (id, enrollment, pass)
 VALUES
 (1, '1', '$2a$10$nAJ2Cfmr2nT0/vcYaQUD2eX7hZOmYTVp7PzO62ot2/b3WffES8Pf6');
-
 INSERT INTO tb_user_role
 (id, user_id, role_id)
 VALUES
