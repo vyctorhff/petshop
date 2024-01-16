7430b2ba28b3b4b45422fa714e47dd907bce2ee1
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-01-16 08:14:36 -0300

    feat(ms-auth): add security filter

--- ms-auth/src/main/java/br/com/petshop/auth/config/SecurityConfig.java
+++ ms-auth/src/main/java/br/com/petshop/auth/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package br.com.petshop.auth.config;
 
+import br.com.petshop.auth.infra.security.TokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,10 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private TokenFilter tokenFilter;
 
     @Bean
     SecurityFilterChain security(HttpSecurity httpSecurity) throws Exception {
@@ -28,6 +34,7 @@ public class SecurityConfig {
 
                 registry.anyRequest().authenticated();
             })
+            .addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
