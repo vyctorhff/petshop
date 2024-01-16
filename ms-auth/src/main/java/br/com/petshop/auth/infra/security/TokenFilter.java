79251257f62ceeb90aa140c69d27a29479fa36e4
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-01-16 08:15:18 -0300

    feat(ms-auth): security filter for token jwt

---
+++ ms-auth/src/main/java/br/com/petshop/auth/infra/security/TokenFilter.java
@@ -0,0 +1,34 @@
+package br.com.petshop.auth.infra.security;
+
+import br.com.petshop.auth.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class TokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+
+        if (token != null) {
+            token.replace("Bearer ", StringUtils.EMPTY);
+            tokenService.validate(token);
+            // TODO: check on databaes the enrollment
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
