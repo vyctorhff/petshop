1c4518f847e7f227f67a1052d3653433845b6af2
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-01-24 09:02:47 -0300

    feat(ms-auth): sequence enrollment repository

---
+++ ms-auth/src/main/java/br/com/petshop/auth/infra/security/EnrollmentNextValueRepository.java
@@ -0,0 +1,19 @@
+package br.com.petshop.auth.infra.security;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class EnrollmentNextValueRepository {
+
+    private final EntityManager entityManager;
+
+    public Integer next() {
+        Query query = entityManager.createNativeQuery("SELECT nextval('seq_user_enrollment')");
+        Long value = (Long) query.getSingleResult();
+        return value.intValue();
+    }
+}
