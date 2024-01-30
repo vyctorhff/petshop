14d5f52270c8191821309b9255a8139303639ed0
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-01-30 08:49:50 -0300

    fix(ms-auth): use sequence on migration

--- ms-auth/src/main/resources/db/migration/V1_0_7__INSERT_USER_ADMIN.sql
+++ ms-auth/src/main/resources/db/migration/V1_0_7__INSERT_USER_ADMIN.sql
@@ -2,9 +2,10 @@ INSERT INTO tb_user
 (id, enrollment, pass)
 VALUES
 (1, '1', '$2a$10$nAJ2Cfmr2nT0/vcYaQUD2eX7hZOmYTVp7PzO62ot2/b3WffES8Pf6');
+
 INSERT INTO tb_user_role
-(id, user_id, role_id)
+(user_id, role_id)
 VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 1, 3);
+(1, 1),
+(1, 2),
+(1, 3);
