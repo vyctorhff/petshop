c3cd8856e3d0be6e2193b864713ac67593bf5f21
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-02-16 11:15:27 -0300

    feat(ms-auth): add controller and repository for role

--- ms-auth/src/main/java/br/com/petshop/auth/controllers/RoleControllerV1.java
+++ ms-auth/src/main/java/br/com/petshop/auth/controllers/RoleControllerV1.java
@@ -1,5 +1,31 @@
 package br.com.petshop.auth.controllers;
 
+import br.com.petshop.auth.infra.RoleRepository;
+import br.com.petshop.auth.model.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("role/v1")
+@RequiredArgsConstructor
 public class RoleControllerV1 {
-    // TODO: make it
+
+    private final RoleRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Role>> getAll() {
+        // TODO: create dto
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/name")
+    public ResponseEntity<Role> findByName(String name) {
+        // TODO: create dto
+        return ResponseEntity.ok(repository.findRoleByName(name));
+    }
 }
--- ms-auth/src/main/java/br/com/petshop/auth/infra/RoleRepository.java
+++ ms-auth/src/main/java/br/com/petshop/auth/infra/RoleRepository.java
@@ -4,10 +4,8 @@ import br.com.petshop.auth.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
-    List<Role> findByName(String name);
+    Role findRoleByName(String name);
 }
