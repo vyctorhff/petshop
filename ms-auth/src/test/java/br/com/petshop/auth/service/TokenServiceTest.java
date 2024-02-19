46843084f4521945075592095cffcf0253e1bf0e
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-02-19 09:37:21 -0300

    test(ms-auth): add teste for token service

---
+++ ms-auth/src/test/java/br/com/petshop/auth/service/TokenServiceTest.java
@@ -0,0 +1,53 @@
+package br.com.petshop.auth.service;
+
+import br.com.petshop.auth.model.User;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith({MockitoExtension.class})
+class TokenServiceTest {
+
+    @InjectMocks
+    private TokenService sut;
+
+    @BeforeEach
+    void setUp() {
+        ReflectionTestUtils.setField(sut, "jwtSecret", "jwt-key-secret");
+    }
+
+    @Test
+    void shouldGenerate() {
+        User user = getUser();
+        String token = sut.generate(user);
+
+        assertNotNull(token);
+        assertFalse(StringUtils.isBlank(token));
+    }
+
+    @Test
+    void shouldValidate() {
+        User user = getUser();
+        String token = sut.generate(user);
+
+        String validate = sut.validate(token);
+
+        assertFalse(StringUtils.isBlank(validate));
+        assertEquals("2", validate);
+    }
+
+    private static User getUser() {
+        User user = new User();
+        user.setName("name");
+        user.setAlias("alias");
+        user.setEnrollment(2);
+        user.setPass("1234");
+        return user;
+    }
+}
