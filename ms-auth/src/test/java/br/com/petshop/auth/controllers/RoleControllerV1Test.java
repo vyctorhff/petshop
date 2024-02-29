9113092aec94ec2c96b3fe36cbcac46c6774107d
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-02-29 17:55:52 -0300

    test(ms-auth): roler controller

--- ms-auth/src/test/java/br/com/petshop/auth/controllers/RoleControllerV1Test.java
+++ ms-auth/src/test/java/br/com/petshop/auth/controllers/RoleControllerV1Test.java
@@ -1,12 +1,16 @@
 package br.com.petshop.auth.controllers;
 
 import br.com.petshop.auth.infra.RoleRepository;
+import br.com.petshop.auth.services.TokenService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(RoleControllerV1.class)
 class RoleControllerV1Test {
@@ -17,5 +21,13 @@ class RoleControllerV1Test {
     @MockBean
     private RoleRepository repository;
 
-    // TODO: working progress
+    @MockBean
+    private TokenService tokenService;
+
+    @Test
+    void shouldGetAll() throws Exception {
+        // FIXME: error test
+        mockMvc.perform(MockMvcRequestBuilders.get("/role/v1"))
+            .andExpect(status().isOk());
+    }
 }
