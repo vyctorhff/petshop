e582f58ed53072b2da07f9c568e1c28c2072077c
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-03-04 10:22:34 -0300

    feat(ms-auth): load and add to spring context

--- ms-auth/src/main/java/br/com/petshop/auth/infra/security/AuthenticatioService.java
+++ ms-auth/src/main/java/br/com/petshop/auth/infra/security/AuthenticatioService.java
@@ -3,6 +3,8 @@ package br.com.petshop.auth.infra.security;
 import br.com.petshop.auth.infra.UserRepository;
 import br.com.petshop.auth.model.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,5 +20,12 @@ public class AuthenticatioService implements UserDetailsService {
     public UserDetails loadUserByUsername(String enrollment) throws UsernameNotFoundException {
         User user = repository.findByEnrollment(Integer.parseInt(enrollment));
         return new UserAuthentication(user);
+    }
+
+    public void setSpringUserAuth(String enrollment) throws UsernameNotFoundException {
+        UserDetails userDetails = this.loadUserByUsername(enrollment);
+        var userAuth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(userAuth);
     }
 }
