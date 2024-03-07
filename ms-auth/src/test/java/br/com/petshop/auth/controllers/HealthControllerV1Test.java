1494b7b80668fea929b4e0cbb80ce21b6abbdb53
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-03-07 09:34:10 -0300

    test(ms-auth): healt controller

--- ms-auth/src/test/java/br/com/petshop/auth/controllers/HealthControllerV1Test.java
+++ ms-auth/src/test/java/br/com/petshop/auth/controllers/HealthControllerV1Test.java
@@ -9,8 +9,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
