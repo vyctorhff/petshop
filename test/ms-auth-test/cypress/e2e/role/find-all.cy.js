b3e56de2d947c5498fb9cf58e2af922280d7bbdd
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-06-04 17:42:31 -0300

    feat(ms-auth-test): role test - working progress

--- test/ms-auth-test/cypress/e2e/role/find-all.cy.js
+++ test/ms-auth-test/cypress/e2e/role/find-all.cy.js
@@ -1,8 +1,18 @@
-const url = 'http://localhost:8080'
+const root = 'http://localhost:8080'
 
 describe('Role: find all', () => {
 
-    const endpoint = `${url}/role/v1`
+    const baseUrl = `${root}/role/v1`
 
-    it("test 1", () => {})
+    it("find all", () => {
+        // get baseUrl
+    })
+
+    it("find by name", () => {
+        const name = 'person'
+        const url = `${baseUrl}/find/${name}/name`
+
+        // get by name
+        // response: name
+    })
 })
