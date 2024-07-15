347482ded37b1665694d9200329cf02dc85a4e2e
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-07-15 09:03:26 -0300

    test(ms-auth-test): add test for token - in progress

--- test/ms-auth-test/cypress/e2e/login/token.cy.js
+++ test/ms-auth-test/cypress/e2e/login/token.cy.js
@@ -1,3 +1,8 @@
 // TODO: extract to one place
 const rootUrl = 'http://localhost:8080'
 const baseUrl = `${rootUrl}/token/v1`
+
+describe('token - in progress', () => {
+
+    it('token - in progress', () => {})
+})
