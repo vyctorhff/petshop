122d14c4c8ac709f99f548a30465fb7d936ff6d1
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-07-15 09:06:06 -0300

    test(ms-auth-test): add test for auth - in progress

--- test/ms-auth-test/cypress/e2e/login/auth.cy.js
+++ test/ms-auth-test/cypress/e2e/login/auth.cy.js
@@ -1,3 +1,8 @@
 // TODO: extract to one place
 const rootUrl = 'http://localhost:8080'
 const baseUrl = `${rootUrl}/auth/v1`
+
+describe('auth - in progress', () => {
+
+    it('auth - in progress', () => {})
+})
