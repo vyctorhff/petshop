b69190a1d07c8ad06999fcc3ca6ff335d58b079c
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-07-19 09:37:53 -0300

    test(ms-auth-test): add test for auth

--- test/ms-auth-test/cypress/e2e/login/auth.cy.js
+++ test/ms-auth-test/cypress/e2e/login/auth.cy.js
@@ -4,5 +4,21 @@ const baseUrl = `${rootUrl}/auth/v1`
 
 describe('auth - in progress', () => {
 
-    it('auth - in progress', () => {})
+    it('auth - in progress', () => {
+        const reqBody = {
+            enrollment: '',
+            password: ''
+        }
+
+        cy.request('GET', baseUrl, reqBody).then((resp) => {
+            const {time, token, refresh} = resp.body
+
+            expect(token).not.to.equal('')
+            expect(token).not.to.equal(' ')
+
+            expect(time).not.to.equal('')
+
+            expect(refresh).not.to.equal('')
+        })
+    })
 })
