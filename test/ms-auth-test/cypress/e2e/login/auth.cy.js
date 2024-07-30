c71ccc6be8b61d3eb369befdfd4892b906fc6a28
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-07-30 09:14:03 -0300

    refact(ms-auth-test): refact to use en

--- test/ms-auth-test/cypress/e2e/login/auth.cy.js
+++ test/ms-auth-test/cypress/e2e/login/auth.cy.js
@@ -1,17 +1,23 @@
 // TODO: extract to one place
-const rootUrl = 'http://localhost:8080'
-const baseUrl = `${rootUrl}/auth/v1`
+// const rootUrl = 'http://localhost:8080'
+// const baseUrl = `${rootUrl}/auth/v1`
+
+const authApi = `${Cypress.env('baseUrl')}/auth/v1`
 
 describe('auth - in progress', () => {
 
     it('auth - in progress', () => {
+        // TODO: update to config env
         const userAdmin = {
             enrollment: '1',
             password: '123456'
         }
 
-        cy.request('GET', baseUrl, userAdmin).then((resp) => {
+        // cy.request('GET', baseUrl, userAdmin).then((resp) => {
+        cy.request('GET', authApi, userAdmin).then((resp) => {
             const {time, token, refresh} = resp.body
+
+            // expect(token).not.to.be.oneOf([undefined, null, '', ' '])
 
             expect(token).not.to.equal(' ')
             expect(token).not.to.equal('')
