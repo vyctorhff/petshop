383cbf80c0b2cdf0da6283cbde6df84093527302
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2024-08-12 18:12:13 -0300

    test(ms-auth-test): check refresh token

--- test/ms-auth-test/cypress/e2e/login/token.cy.js
+++ test/ms-auth-test/cypress/e2e/login/token.cy.js
@@ -1,13 +1,19 @@
-// TODO: extract to one place
-const rootUrl = 'http://localhost:8080'
-const baseUrl = `${rootUrl}/token/v1`
+const tokenApi = `${Cypress.env('baseUrl')}/auth/v1`
 
 describe('token', () => {
 
     it('refresh token', () => {
         cy.doLoginAdmin().then((resp) => {
-            cy.log('passou por aqui')
-            cy.log(resp)
+            const token = resp.body.token
+
+            cy.request('PATCH', tokenApi, requestBody).then((resp) => {
+                const body = resp.body
+
+                expect(body.refresh).not.to.be.oneOf([undefined, null, '', ' '])
+                expect(body.token).not.to.be.oneOf([undefined, null, '', ' '])
+
+                expect(body.token).not.eq(token)
+            })
         })
     })
 })
